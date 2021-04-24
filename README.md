ce2d5ce4723280efc1d47f6c2baf7dff8e493b72
Luissso1 <renan.osero140412@my.lsgh.edu.ph>
2021-04-24 17:48:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pe-exam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pe-exam)
---
+++ pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html"
+})
+export class HomePage {
+  original_price: number;
+  discounted_price:number;
+  percent_off: number;
+  sales_tax: number;
+  finalprice: number;
+  savings: number;
+  tax:number; 
+
+  constructor(public navCtrl: NavController) {}
+
+  calculatePERCENTOFF() {
+    this.discounted_price = this.original_price * (1-this.percent_off/100);
+    this.tax = this.discounted_price * this.sales_tax/100;
+    this.finalprice = this.discounted_price + this.tax;
+    this.savings = (this.original_price - this.finalprice) + this.original_price * this.sales_tax/100;
+
+  }
+}
